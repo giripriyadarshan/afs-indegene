7fb54ae55359802faf04839dae1bc3c133b392e0
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2023-05-25 23:32:56 +0530

    no more thumb images .... only compress and upload

--- afs-cli/src/args.rs
+++ afs-cli/src/args.rs
@@ -30,20 +30,4 @@ pub struct Upload {
     #[clap(short, long, required = true)]
     /// array of key_messages
     key_messages: Vec<String>,
-
-    #[clap(short, long)]
-    /// if thumb is true, will generate thumb
-    thumb: bool,
-
-    #[clap(short = 'w', long, default_value = "0")]
-    /// if thumb is true, will accept thumb_width
-    thumb_width: u16,
-
-    #[clap(short = 'e', long, default_value = "0")]
-    /// if thumb is true, will accept thumb_height
-    thumb_height: u16,
-
-    #[clap(short = 'm', long, default_value = "pdf")]
-    /// if "pdf" will generate using pdf file, if "browser" will generate using browser backend
-    thumb_method: String,
 }
