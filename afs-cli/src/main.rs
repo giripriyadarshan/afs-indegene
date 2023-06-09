b26d7141eae19ba1295df8dfbd8c8d6da75c5125
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2023-06-09 15:27:50 +0530

    spawn compress folder process asynchronously

--- afs-cli/src/main.rs
+++ afs-cli/src/main.rs
@@ -6,27 +6,68 @@ mod models;
 mod utils;
 
 use models::{args, config};
+use tokio::task::JoinSet;
+use utils::{compress::compress_folder_contents, get_key_messages::get_key_messages};
 
-fn main() {
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
     // check if config.toml exists
-    // if !std::path::Path::new("config.toml").exists() {
-    //     eprintln!("config.toml not found");
-    //     std::process::exit(1);
-    // }
+    if !std::path::Path::new("config.toml").exists() {
+        eprintln!("config.toml not found");
+        std::process::exit(1);
+    }
 
     let args = args::Arguments::parse();
     println!("{:?}", args);
-    // let mut file = File::open("config.toml").unwrap();
-    // let mut contents = String::new();
-    // file.read_to_string(&mut contents).expect("Failed to read config file");
-    // let config: config::Config = toml::from_str(contents.as_str()).unwrap();
+    let mut file = File::open("config.toml").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Failed to read config file");
+    let config: config::Config = toml::from_str(contents.as_str()).unwrap();
 
     // println!("{:?} {:?}", args, config);
 
-    let folder_path = args.run_code.as_str();
+    let key_messages = get_key_messages(
+        config.files.key_messages_file,
+        "deliverable_path".to_owned(),
+    );
 
-    match utils::compress::compress_folder_contents(folder_path, "zip_file_path") {
-        Ok(()) => println!("Archive created successfully"),
-        Err(e) => eprintln!("Error creating archive: {}", e),
+    match key_messages {
+        Some(key_messages) => {
+            std::fs::create_dir("output").unwrap();
+            let mut processes = JoinSet::new();
+            for km in key_messages {
+                let output = format!("output/{}.zip", km);
+                processes.spawn(async move {
+                    // check if km folder exists
+                    if !std::path::Path::new(km.as_str()).exists() {
+                        eprintln!("{} not found", km);
+                        std::process::exit(1);
+                    }
+                    // compress the km
+                    compress_folder_contents(km, output);
+                });
+            }
+
+            while let Some(res) = processes.join_next().await {
+                if res.is_err() {
+                    eprintln!("Error: {:?}", res);
+                }
+            }
+            // delete the output folder
+            std::fs::remove_dir_all("output").unwrap();
+        }
+        None => {
+            println!("No new key messages");
+        }
     }
+
+    // let folder_path = args.run_code.as_str();
+
+    // match utils::compress::compress_folder_contents(folder_path, "zip_file_path").await {
+    //     Ok(()) => println!("Archive created successfully"),
+    //     Err(e) => eprintln!("Error creating archive: {}", e),
+    // }
+
+    Ok(())
 }
--- afs-cli/src/utils/compress.rs
+++ afs-cli/src/utils/compress.rs
@@ -1,10 +1,11 @@
 use std::fs::File;
 use walkdir::WalkDir;
-use zip::{result::ZipError, ZipWriter};
+use zip::ZipWriter;
 
-pub fn compress_folder_contents(folder_path: &str, zip_file_path: &str) -> Result<(), ZipError> {
+pub fn compress_folder_contents(folder_path: String, zip_file_path: String) {
+    let folder_path = folder_path.as_str();
     // Create a new zip file at the specified path
-    let zip_file = File::create(zip_file_path)?;
+    let zip_file = File::create(zip_file_path).unwrap();
     let mut zip_writer = ZipWriter::new(zip_file);
 
     // Recursively iterate over each file and folder in the specified folder and add them to the zip archive
@@ -15,23 +16,26 @@ pub fn compress_folder_contents(folder_path: &str, zip_file_path: &str) -> Resul
         if entry_path.is_file() {
             // If the path is a file, add it to the archive
             let relative_path = entry_path.strip_prefix(folder_path).unwrap();
-            let mut file = File::open(entry_path)?;
-            zip_writer.start_file(
-                relative_path.to_str().unwrap().to_owned(),
-                Default::default(),
-            )?;
-            std::io::copy(&mut file, &mut zip_writer)?;
+            let mut file = File::open(entry_path).unwrap();
+            zip_writer
+                .start_file(
+                    relative_path.to_str().unwrap().to_owned(),
+                    Default::default(),
+                )
+                .unwrap();
+            std::io::copy(&mut file, &mut zip_writer).unwrap();
         } else if entry_path.is_dir() {
             // If the path is a directory, create a directory entry in the archive
             let relative_path = entry_path.strip_prefix(folder_path).unwrap();
-            zip_writer.add_directory(
-                relative_path.to_str().unwrap().to_owned(),
-                Default::default(),
-            )?;
+            zip_writer
+                .add_directory(
+                    relative_path.to_str().unwrap().to_owned(),
+                    Default::default(),
+                )
+                .unwrap();
         }
     }
 
-    // Finish writing the zip file and return Ok if successful
-    zip_writer.finish()?;
-    Ok(())
+    // Finish writing the zip file
+    zip_writer.finish().unwrap();
 }
