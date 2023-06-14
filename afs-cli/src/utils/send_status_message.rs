75dc776a2dd1d5d9e506422aa7c56e6c0c2d5030
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2023-06-14 15:19:27 +0530

    url edit

--- afs-cli/src/utils/send_status_message.rs
+++ afs-cli/src/utils/send_status_message.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 pub async fn send_message(run_code: Arc<String>, message: String) {
     let client = Client::new();
     client
-        .post("http://172.30.106.6:3825")
+        .post("http://localhost:3825")
         .header(CONTENT_TYPE, "application/json")
         .body(
             serde_json::to_string(&serde_json::json!({
