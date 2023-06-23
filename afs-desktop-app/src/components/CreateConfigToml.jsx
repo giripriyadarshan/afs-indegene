3f20a59265c14b1f8862a30bb60fc1d3c7e326b9
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2023-06-23 15:54:04 +0530

    config creation form setup

--- afs-desktop-app/src/components/CreateConfigToml.jsx
+++ afs-desktop-app/src/components/CreateConfigToml.jsx
@@ -4,7 +4,9 @@ import Input from './micro/Input'
 import GenericButton from './micro/GenericButton';
 
 export default function CreateConfigToml() {
-    const [testInput, setTestInput] = useState('');
+    const [url, setUrl] = useState('');
+    const [documentLink, setDocumentLink] = useState('');
+    const [documentNumber, setDocumentNumber] = useState('');
     return (
         <>
             <h1>Veeva Vault Uploader Config Creator</h1>
@@ -14,11 +16,33 @@ export default function CreateConfigToml() {
                 <div className="input-element">
                     <Input
                         label={"SVN URL"}
-                        name={"ASDASD"}
-                        onChange={(e) => setTestInput(e.target.value)}
+                        name={"svn-url"}
+                        onChange={(e) => setUrl(e.target.value)}
                         placeholder={"Please enter the svn URL of HTML Folder"}
                         type={"text"}
-                        value={testInput}
+                        value={url}
+                    />
+                </div>
+
+                <div className="input-element">
+                    <Input
+                        label={"Document Link"}
+                        name={"document-link"}
+                        onChange={(e) => setDocumentLink(e.target.value)}
+                        placeholder={"Please enter the document link"}
+                        type={"text"}
+                        value={documentLink}
+                    />
+                </div>
+
+                <div className="input-element">
+                    <Input
+                        label={"Document Number"}
+                        name={"document-number"}
+                        onChange={(e) => setDocumentNumber(e.target.value)}
+                        placeholder={"Please enter the document number"}
+                        type={"number"}
+                        value={documentNumber}
                     />
                 </div>
 
