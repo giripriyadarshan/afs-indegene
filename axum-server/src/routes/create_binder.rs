3d53a12044424f63e19e6e8bf592ef60d060debb
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2023-09-22 17:06:14 +0530

    backup commit

--- axum-server/src/routes/create_binder.rs
+++ axum-server/src/routes/create_binder.rs
@@ -1,3 +1,24 @@
-pub async fn create_binder() -> String {
+use axum::response::Json;
+use serde_json::{json, Value};
+use std::collections::HashMap;
+use url::Url;
+
+use reqwest::{header::AUTHORIZATION, Client};
+
+use crate::utils::{
+    encode_json::encode_json, extract_string::ExtractString, session_id::get_session_id,
+};
+
+pub async fn create_binder(body: String) -> String {
+    let body: Value = serde_json::from_str(&body).unwrap();
+
+    let session_id = get_session_id(
+        body["instance"].to_string().remove_d_quotes(),
+        body["account"].to_string().remove_d_quotes(),
+    )
+        .await;
+
+
+
     todo!()
 }
